505de17d3031051c2ebf0821dc718ef07590d3da
Jesus A Hernandez de Rojas <jesusangel.hernandez@uva.es>
2018-05-04 13:47:12 +0200

    Update menu.js
    Updated for when you change the resolution of the page... js responsive :)

--- menu/js/menu.js
+++ menu/js/menu.js
@@ -1,5 +1,7 @@
 // JS for the menu element
 
+window.addEventListener('resize', showallMenuElements);
+
 function createMenuNavigation() {
   var elementsMenu = document.querySelectorAll('.menu .menu_group');
   for (let i = 0; i<elementsMenu.length; i++) {
@@ -11,6 +13,14 @@ function createMenuNavigation() {
       elementsMenu[i].querySelector('ul').classList.remove('hide_element');
       elementsMenu[i].querySelector('ul').classList.add('show_element');
     });
+  }
+}
+
+function showallMenuElements() {
+  var elements = document.querySelectorAll('.menu .menu_group ul');
+  for (var i = 0; i<elements.length; i++) {
+    elements[i].classList.remove('hide_element');
+    elements[i].classList.remove('show_element');
   }
 }
 
