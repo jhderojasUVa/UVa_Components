60a8e54e3b6263c091ebfd1424f45c8a2d785808
Jesus A Hernandez de Rojas <jesusangel.hernandez@uva.es>
2018-05-07 10:20:40 +0200

    Waiting for the menu UI
    UX for the menu option in made. Waiting for the UI

--- menu/js/menu.js
+++ menu/js/menu.js
@@ -3,6 +3,7 @@
 window.addEventListener('resize', showallMenuElements);
 
 function createMenuNavigation() {
+  // Funcion que crea la funcionalidad de movil del menu
   var elementsMenu = document.querySelectorAll('.menu .menu_group');
   for (let i = 0; i<elementsMenu.length; i++) {
     elementsMenu[i].querySelector('h2').addEventListener('click', function(event){
@@ -17,10 +18,24 @@ function createMenuNavigation() {
 }
 
 function showallMenuElements() {
+  // Funcion que elimina los CSS de createMenuNavigation si se cambia de tamaño
   var elements = document.querySelectorAll('.menu .menu_group ul');
   for (var i = 0; i<elements.length; i++) {
     elements[i].classList.remove('hide_element');
     elements[i].classList.remove('show_element');
+  }
+}
+
+function showHideElement(element) {
+  // Funcion que oculta o muestra un elemento
+  // element: elemento (class)
+  var theElement = document.querySelectorAll('.'+element);
+  for (let i = 0; i<theElement.length; i++) {
+    if (theElement[i].style.display === 'none') {
+      theElement[i].style.display = 'block';
+    } else {
+      theElement[i].style.display = 'none';
+    }
   }
 }
 
